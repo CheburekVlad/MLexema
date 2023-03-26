841de30590c6a21141c7dd78bd50728ef8841e79
lea-mhx <124294045+lea-mhx@users.noreply.github.com>
2023-03-26 14:42:05 +0200

    change layout test

--- ui/fileTab.R
+++ ui/fileTab.R
@@ -4,7 +4,6 @@
   actionButton("load", "Chargement modèle"),
 
   # Choix du type de données
-  radioButtons("radio", label = "Format de fichier:", choices = list("Données brutes" = 1, "Données fold change" = 2), selected = 1),
   numericInput("numPartition", "Pourcentage de partition", value = 0.75, min = 0, step = 0.05, max = 1),
 
   br(),br(),
