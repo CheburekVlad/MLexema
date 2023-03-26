ad79bfc020deca4dee0a41e3c1f97b9c858431ef
lea-mhx <124294045+lea-mhx@users.noreply.github.com>
2023-03-26 14:41:38 +0200

    change of the layout test

--- app.R
+++ app.R
@@ -8,6 +8,7 @@ source(file.path('ui', 'fileTab.R'))
 source(file.path('ui', 'algoTab.R'))
 source(file.path('ui', 'resultTab.R'))
 source(file.path('ui', 'predictionTab.R'))
+source(file.path('ui', 'preproTab.R'))
 
 ui <- dashboardPage(
   dashboardHeader(title = "Molecular diagnosis"),
@@ -15,7 +16,10 @@ ui <- dashboardPage(
   dashboardSidebar(
     sidebarMenu(
       id = "tabs",
-      menuItem("Fichiers",  icon = icon("folder"), tabName = "fileTab"),
+      menuItem("Fichiers",  icon = icon("folder"), startExpanded = TRUE,
+        menuSubItem("Normalisation données brute", tabName = "preproTab"),
+        menuSubItem("Chargement données", tabName = "fileTab")
+      ),
       
       menuItem("Entraînement", icon = icon("gear"), startExpanded = TRUE,
         menuSubItem("Algorithme", tabName = "algoTab"),
@@ -30,6 +34,7 @@ ui <- dashboardPage(
       style='padding:2em;',
       tabItems(
         tabItem(tabName = "fileTab",   fileLayout),
+        tabItem(tabName = "preproTab", preproLayout),
         tabItem(tabName = "algoTab",   algoLayout),
         tabItem(tabName = "resultTab", resultsLayout),
         tabItem(tabName = "predictionTab", predictionLayout)
@@ -47,12 +52,14 @@ source(file.path('backend', 'fileBackend.R'))
 source(file.path('backend', 'algoBackend.R'))
 source(file.path('backend', 'resultBackend.R'))
 source(file.path('backend', 'predictionBackend.R'))
+source(file.path('backend', 'preproBackend.R'))
 
 server <- function(input, output, session) {
   fileBackend(input, output, session)
   algoBackend(input, output, session)
   resultBackend(input, output, session)
   predictionBackend(input, output, session)
+  preproBackend(input, output)
 }
 
 
