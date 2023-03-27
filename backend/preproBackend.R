47bef2403d596bb6ef08292814470931d7bc1497
lea-mhx <124294045+lea-mhx@users.noreply.github.com>
2023-03-27 21:08:32 +0200

    traduction des commentaires en français

--- backend/preproBackend.R
+++ backend/preproBackend.R
@@ -1,8 +1,8 @@
 preproBackend <- function(input, output) {
   
-  ######################################### Loading and display ###################################################
+  ######################################### chargement des données et présentation ###################################################
   
-  # Load the CSV file into a dataframe
+  # Chargement d'un fichier csv contenant les données
   data <- reactive({
     req(input$file1)
     df <- read.csv(input$file1$datapath, header = TRUE, stringsAsFactors = FALSE, sep =';',
@@ -10,50 +10,50 @@ preproBackend <- function(input, output) {
     df
   })
   
-  # Load the data from file 2
+  # Chargement d'un fichier csv avec les efficacités
   efficiency <- reactive({
     req(input$file2)
     eff <- read.csv(input$file2$datapath, header = TRUE)
     eff
   })
   
-  # Display raw data
+  # Permet de faire apparaîtres les données
   output$table1 <- renderDT({
     data()
   })
   
-  # Display efficiency data
+  # Permer de faire apparaitres les efficacités
   output$table2 <- renderDT({
     efficiency()
   })
   
   ###############################################################################################################################
   
-  ####################################### Selection of the columns #####################################################################
+  ####################################### Sélection des catégories de colonnes #####################################################################
   
   
-  # Update the select input for info columns based on the loaded data
+  # Mise à jour des colonnes selectionnés, cas des colonnes avec informations
   output$column_selector_1 <- renderUI({
     req(data())
-    selectInput("column_select_1", "Select a Column for Group 1:", choices = colnames(data()), multiple = TRUE)
+    selectInput("column_select_1", "Selection colonnes informatives:", choices = colnames(data()), multiple = TRUE)
   })
   
-  # Update the select input for gene columns based on the loaded data
+  # Mise à jour des colonnes selectionnées, cas de colonnes avec l'expressions des gènes
   output$column_selector_2 <- renderUI({
     req(data())
-    selectInput("column_select_2", "Select a Column for Group 2:", choices = colnames(data()), multiple = TRUE)
+    selectInput("column_select_2", "Selection des colonnes avec expressions des gènes :", choices = colnames(data()), multiple = TRUE)
   })
   
   
   
   ####################################################################################################################################
-  ################################ Call of the function and display of the result  ####################################################@
+  ################################ Appelle des fonctions et présentation des résultats  ####################################################@
   
-  # Compute means based on selected columns when button is clicked
+  # Active le processus de normalisation après appui du bouton 
   observeEvent(input$compute, {
     req(input$column_select_1, input$column_select_2)
     
-    # Select the data columns and id columns
+    # Enregistre les indices des colonnes selectionnés plus haut
     info_cols <- input$column_select_1
     gene_cols <- input$column_select_2
     
@@ -62,14 +62,14 @@ preproBackend <- function(input, output) {
     liste_info <- which(colnames(df) %in% info_cols)
     liste_gene <- which(colnames(df) %in% gene_cols)
     
-    # Compute the mean columns
+    # Normalisation des données avec les fonctions 
     mean_df <- compute_mean_columns(df, liste_info, liste_gene)
     efficiency_df <- normalize_by_efficiency(mean_df, eff, liste_gene)
     efficiency_df$mean_GAPDH_actine <- (efficiency_df$GAPDH + efficiency_df$Actine)/2
     normalized_df <- normalized_by_ref_gene(efficiency_df, liste_info, liste_gene)
     result_dt <- sample_ratio(normalized_df, liste_info)
     
-    # Display the result dataframe
+    # Affichage du tableau de resultat
     output$table3 <- renderTable(result_dt)
   })
   
